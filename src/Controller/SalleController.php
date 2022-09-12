e6169c1a554874dad38f2167c7f8c200f687ef68
Thomas Thirion <thirionthomas68100@gmail.com>
2022-09-12 12:34:03 +0200

    ajout table administrateur

--- src/Controller/SalleController.php
+++ src/Controller/SalleController.php
@@ -2,15 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Structure;
+
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SalleController extends AbstractController
 {
-    #[Route('/salle', name: 'salle')]
-    public function index(): Response
+
+
+    #[Route('/salle/{id}', name: 'salle')]
+    public function index($id ,ManagerRegistry $managerRegistry): Response
     {
-        return $this->render('salle/index.html.twig');
+        $structure = $managerRegistry->getRepository(Structure::class)->findOneBy(array('id'=>$id));
+        $options = $this->getUser()->getOption();
+        $name = $this->getUser()->getName();
+
+        if(!$structure){
+            return $this->redirectToRoute('franchise');
+        }
+
+        return $this->render('salle/index.html.twig',[
+            'structure' => $structure,
+            'option' => $options,
+            'user' => $name
+        ]);
+
     }
 }
