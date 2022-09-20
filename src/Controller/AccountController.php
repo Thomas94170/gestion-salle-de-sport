14a0969e0290d925de368e90033cf5f32909532c
Thomas Thirion <thirionthomas68100@gmail.com>
2022-09-20 19:36:06 +0200

    mon compte debut info ok resoudre debug  first connexion

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,6 +13,16 @@ class AccountController extends AbstractController
     #[Route('/mon-compte', name: 'mon-compte')]
     public function index(): Response
     {
-        return $this->render('account/index.html.twig');
+
+        $getUser = $this->getUser();
+
+
+        return $this->render('account/index.html.twig',[
+            'user'=>$getUser,
+
+
+        ]);
+
+
     }
 }
